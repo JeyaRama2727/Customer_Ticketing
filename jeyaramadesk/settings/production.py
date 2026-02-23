28c985089c1995b0efb1008663ccec3a1f8bfc4e
Jerish4344 <jerish220@gmail.com>
2026-02-23 14:22:04 +0530

    Remove innodb_strict_mode from prod settings

--- jeyaramadesk/settings/production.py
+++ jeyaramadesk/settings/production.py
@@ -31,7 +31,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', '3306'),
         'OPTIONS': {
             'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
